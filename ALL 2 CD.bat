eaf28139dfb10bea9ef5ed7e12b3492f52c1b8b7
feeshy <10860212+feeshy@users.noreply.github.com>
2018-08-18 15:32:23 +0800

    Update and rename AUNE_M1_CONVERTER.bat to ALL 2 CD.bat

---
+++ ALL 2 CD.bat
@@ -0,0 +1,7 @@
+@echo off
+echo 将转换本文件夹内所有扩展名为flac、ape、m4a、aac、mp3的文件至16bit 44100kHz编码的wav
+echo 如有其他需求，请右键编辑本批处理，自行增删 :P
+pause
+for %%I IN ("*.flac" "*.ape" "*.m4a" "*.aac" "*.mp3") DO ffmpeg -i "%%I" -sample_fmt s16 -ar 44100 "%%~nI".wav
+echo 已全部转换为 16bit 44100kHz WAV
+pause
